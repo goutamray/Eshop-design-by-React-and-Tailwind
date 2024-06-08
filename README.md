ac38f3ee48d6f17b921ad3e530d327b6c6a06b59
goutam <goutamr362@gmail.com>
2024-06-08 11:00:38 +0600

    react install & tailwind css setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+React With Tailwind Css Basic design 
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <h2 className="text-center"> Hello </h2>
+    </>
+  )
+}
+
+export default App
